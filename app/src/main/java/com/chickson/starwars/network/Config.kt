10acbe392a972e12e94f61f8c697a8c50e0a2c66
chiCKson <erandraj@gmail.com>
2022-06-17 16:37:41 +0530

    Gradle: Loaded Dependencies

--- app/src/main/java/com/chickson/starwars/network/Config.kt
+++ app/src/main/java/com/chickson/starwars/network/Config.kt
@@ -1,4 +1,41 @@
 package com.chickson.starwars.network
 
-class Config {
+import android.util.Log
+import com.chickson.starwars.BuildConfig
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import javax.inject.Inject
+
+class Config @Inject constructor() {
+    val client = HttpClient(Android) {
+        install(DefaultRequest) {
+            headers.append("Content-Type", "application/json")
+            host = BuildConfig.API_HOST
+            //parameter("apiKey",BuildConfig.NEWS_API_KEY)
+            url {
+                protocol = URLProtocol.HTTPS
+            }
+        }
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Log.v("Logger Ktor =>", message)
+                }
+            }
+            level = LogLevel.ALL
+        }
+        engine {
+            connectTimeout = 100_000
+            socketTimeout = 100_000
+        }
+    }
 }
